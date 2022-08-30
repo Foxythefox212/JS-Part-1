56432394d82832f4a8d2c168e8489a06d82c9058
Damian Ruxandra Laura <Berbecutza1986@gmail.com>
2022-08-30 15:59:15 +0300

    Exercitiu cu Function Declaration si Function Expressin.

--- exersare.js
+++ exersare.js
@@ -9,3 +9,31 @@ const descRomania = describeCountry("Romania", 6000000, "Bucuresti");
 const descGermania = describeCountry("Germania", 83000000, "Berlin");
 const descSpania = describeCountry("Spania", 44000000, "Barcelona");
 console.log(descRomania, descGermania, descSpania);
+
+// FUNCTION DECLARATION vs. FUNCTION EXPRESSIONS
+// 01-Function Declarations
+function percentageOfWorld1(populations) {
+  return (populations / 7900) * 100;
+}
+const firstEuropa = percentageOfWorld1(1100);
+const firstAmerica = percentageOfWorld1(900);
+const firstAustralia = percentageOfWorld1(500);
+console.log(firstEuropa); //13.924050
+console.log(firstAmerica); //11.392405
+console.log(firstAustralia); // 6.32911392
+// JONAS A SCRIS ASA
+const percPortugal1 = percentageOfWorld1(10);
+const percChina1 = percentageOfWorld1(1441);
+const percUSA1 = percentageOfWorld1(332);
+console.log(percPortugal1, percChina1, percUSA1);
+
+// 02-Function Expression
+const percentageOfWorld2 = function (populations) {
+  return (populations / 7900) * 100;
+};
+const secondEuropa = percentageOfWorld2(1100);
+const secondAmerica = percentageOfWorld2(900);
+const secondAustralia = percentageOfWorld2(500);
+console.log(secondEuropa); //13.924
+console.log(secondAmerica); //11.3924
+console.log(secondAustralia); //6.3291
