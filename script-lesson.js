7879808f562162262d90d6a24d6d0b93bc99cba7
Damian Ruxandra Laura <Berbecutza1986@gmail.com>
2022-09-01 10:31:40 +0300

    Function Arrow

--- script-lesson.js
+++ script-lesson.js
@@ -47,3 +47,24 @@ const calcAge2 = function (birthYear) {
 };
 const age2 = calcAge2(1986);
 console.log(age1, age2);
+
+// FUNCTION ARROW
+//-(scrii o sageata si dupa sageata ce vrei sa iti dea inapoi )
+//ii un fel de function expression si nu avem nevoie de paranteze{}
+// daca e doar un statement, daca is mai multe avem nevoie
+// si return se intampla implicit(este inclus)
+const calcAge3 = (birthYear) => 2022 - birthYear; //Sageata
+const age3 = calcAge3(1986);
+console.log(age3);
+
+// Avem un parametru(birthyear) si mai multe statement si cand avem
+//mai multe statement trebuie trecut return...daca avem doar 1 statement
+//ca si deasupra atunci nu mai punem return
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+console.log(yearsUntilRetirement(1986, "Ruxandra"));
+console.log(yearsUntilRetirement(1976, "Nicoleta"));
